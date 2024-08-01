4c6c38bac608c114e20407d6f2319c1cd861e2b4
AntonyosAziz <antonyosaziz2005@gmail.com>
2024-08-01 19:42:34 +0200

    Aktualisieren von changeStuff.js

--- changeStuff.js
+++ changeStuff.js
@@ -1,5 +1,5 @@
 // Array mit ausgewählten Fonts
-const fonts = ['Edit Points Filled', 'The Centurion', 'Typewriter', 'Logotype'];
+const fonts = ['Edit Points Filled', 'The Centurion', 'Moms_typewriter-webfont.ttf', 'Logotype'];
 const imagelist = ['adam.png', 'stepDom.png', 'pyr.png', 'chromeDino.png']; // Liste der Bildpfade
 // Funktion, um den Font zufällig zu ändern
 // Funktion, um jeden Buchstaben in einem <p> Element unterschiedlich zu ändern
