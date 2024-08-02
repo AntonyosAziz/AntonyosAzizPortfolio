d5d5987e6170e93694fd22eea593bb84b339d9b9
AntonyosAziz <antonyosaziz2005@gmail.com>
2024-08-03 01:15:22 +0200

    Update audioplayer.js

--- audioplayer.js
+++ audioplayer.js
@@ -1,12 +1,34 @@
-var audio = document.getElementById("myAudio");
-var hasScrolled = false;
+/*"document.addEventListener("DOMContentLoaded", function() {
+    var audio = document.getElementById("myAudio");
+    var hasScrolled = false;
 
-window.addEventListener("scroll", function() {
-    if (!hasScrolled) {
-        hasScrolled = true;
-        audio.addEventListener("canplay", function() {
-            audio.currentTime = 15;
-            audio.play();
+    if (audio) {
+        window.addEventListener('scroll', function() {
+            if (!hasScrolled) {
+                hasScrolled = true;
+                audio.addEventListener('canplay', function() {
+                    audio.currentTime = 15;
+                    audio.play();
+                });
+            }
+        });
+    }
+});*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    var audio = document.getElementById("myAudio");
+    var hasScrolled = false;
+
+    if (audio) {
+        // Setze die Startzeit
+        audio.currentTime = 15;
+
+        // Starte die Wiedergabe beim ersten Scrollen
+        window.addEventListener('scroll', function() {
+            if (!hasScrolled) {
+                hasScrolled = true;
+                audio.play();
+            }
         });
     }
 });
